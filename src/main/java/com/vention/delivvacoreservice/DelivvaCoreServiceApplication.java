e5b582bcb0838996e95290bfca34cfe262b2e7fe
Shakhzod Elturaev <shahzodelturayev@gmail.com>
2023-11-22 17:14:24 +0500

    first commit

---
+++ src/main/java/com/vention/delivvacoreservice/DelivvaCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vention.delivvacoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivvaCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelivvaCoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vention/delivvacoreservice/DelivvaCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vention.delivvacoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivvaCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
