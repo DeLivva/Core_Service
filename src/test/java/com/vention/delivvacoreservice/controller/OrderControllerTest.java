ae20fc4569df9e0dd7bb50b1d39f65f120722efc
Shakhzod Elturaev <shahzodelturayev@gmail.com>
2023-12-08 17:16:15 +0500

    tests are added

---
+++ src/test/java/com/vention/delivvacoreservice/controller/OrderControllerTest.java
@@ -0,0 +1,109 @@
+package com.vention.delivvacoreservice.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vention.delivvacoreservice.dto.GeolocationDTO;
+import com.vention.delivvacoreservice.dto.request.OrderCreationRequestDTO;
+import com.vention.delivvacoreservice.dto.response.OrderResponseDTO;
+import com.vention.delivvacoreservice.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OrderControllerTest {
+
+    @Mock
+    private OrderService orderService;
+
+    @InjectMocks
+    private OrderController orderController;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(orderController).build();
+    }
+
+    @Test
+    void testCreateOrder() throws Exception {
+        // given
+        OrderCreationRequestDTO orderCreationRequestDTO = new OrderCreationRequestDTO(
+                1L,
+                "test",
+                new GeolocationDTO(23D, 23D),
+                new GeolocationDTO(23D, 43D),
+                "2023-12-12"
+        );
+        OrderResponseDTO responseDTO = mock();
+        // when
+        String jsonData = objectMapper.writeValueAsString(orderCreationRequestDTO);
+        doReturn(responseDTO).when(orderService).createOrder(any());
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/orders/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonData))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+        verify(orderService, times(1)).createOrder(any());
+    }
+
+    @Test
+    void testCreateOrder2() throws Exception {
+        // given
+        OrderCreationRequestDTO orderCreationRequestDTO = new OrderCreationRequestDTO(
+                1L,
+                "test",
+                null,
+                new GeolocationDTO(23D, 43D),
+                "2023-12-12"
+        );
+        // when
+        String jsonData = objectMapper.writeValueAsString(orderCreationRequestDTO);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/orders/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonData))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        verify(orderService, never()).createOrder(any());
+    }
+
+    @Test
+    void testCreateOrder3() throws Exception {
+        // given
+        OrderCreationRequestDTO orderCreationRequestDTO = new OrderCreationRequestDTO(
+                1L,
+                "test",
+                new GeolocationDTO(23D, 23D),
+                new GeolocationDTO(23D, 43D),
+                "2023/12/12"
+        );
+        // when
+        String jsonData = objectMapper.writeValueAsString(orderCreationRequestDTO);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/orders/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonData))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        verify(orderService, never()).createOrder(any());
+    }
+}
---
+++ src/test/java/com/vention/delivvacoreservice/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,113 @@
+package com.vention.delivvacoreservice.service.impl;
+
+import com.vention.delivvacoreservice.domain.Order;
+import com.vention.delivvacoreservice.domain.OrderDestination;
+import com.vention.delivvacoreservice.dto.GeolocationDTO;
+import com.vention.delivvacoreservice.dto.request.OrderCreationRequestDTO;
+import com.vention.delivvacoreservice.dto.response.OrderResponseDTO;
+import com.vention.delivvacoreservice.dto.response.UserResponseDTO;
+import com.vention.delivvacoreservice.exception.BadRequestException;
+import com.vention.delivvacoreservice.feign_clients.UserClient;
+import com.vention.delivvacoreservice.mappers.OrderMapper;
+import com.vention.delivvacoreservice.repository.OrderRepository;
+import com.vention.delivvacoreservice.service.OrderDestinationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceImplTest {
+
+    @Mock
+    private UserClient userClient;
+
+    @Mock
+    private OrderMapper orderMapper;
+
+    @Mock
+    private TrackNumberGenerator trackNumberGenerator;
+
+    @Mock
+    private OrderDestinationService orderDestinationService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private OrderServiceImpl orderService;
+
+    @Mock
+    private OrderCreationRequestDTO request;
+
+    @Test
+    void testCreateOrder() {
+        // given
+        GeolocationDTO startingPlaceDTO = mock();
+        GeolocationDTO finalPlaceDTO = mock();
+        UserResponseDTO userResponseDTO = mock();
+        OrderDestination destination = mock();
+        String trackNumber = "test";
+        String date = "2023-12-12";
+        Order order = mock();
+        Order savedOrder = mock();
+        OrderResponseDTO responseDTO = mock();
+        // when
+        doReturn(startingPlaceDTO).when(request).getStartingDestination();
+        doReturn(finalPlaceDTO).when(request).getFinalDestination();
+        doReturn(date).when(request).getScheduledDeliveryDate();
+        doReturn(true).when(orderDestinationService).areDestinationsValid(anyList());
+        doReturn(userResponseDTO).when(userClient).getUserById(anyLong());
+        doReturn(destination).when(orderDestinationService).getOrderDestinationWithValidation(any());
+        doReturn(order).when(orderMapper).mapOrderRequestToEntity(any());
+        doReturn(trackNumber).when(trackNumberGenerator).generateTrackNumber(any(), any());
+        doReturn(savedOrder).when(orderRepository).save(any(Order.class));
+        doReturn(responseDTO).when(orderMapper).mapOrderEntityToResponse(any(Order.class));
+        OrderResponseDTO result = orderService.createOrder(request);
+        // then
+        assertSame(responseDTO, result);
+        verify(request, times(1)).getFinalDestination();
+        verify(request, times(1)).getStartingDestination();
+        verify(orderDestinationService, times(1)).areDestinationsValid(anyList());
+        verify(userClient, times(1)).getUserById(anyLong());
+        verify(orderDestinationService, times(2)).getOrderDestinationWithValidation(any());
+        verify(orderMapper, times(1)).mapOrderRequestToEntity(any());
+        verify(trackNumberGenerator, times(1)).generateTrackNumber(any(), any());
+        verify(orderRepository, times(1)).save(any());
+        verify(orderMapper, times(1)).mapOrderEntityToResponse(any());
+    }
+
+    @Test
+    void WillOrderCreationTestThrow() {
+        // given
+        GeolocationDTO startingPlaceDTO = mock();
+        GeolocationDTO finalPlaceDTO = mock();
+        // when
+        doReturn(startingPlaceDTO).when(request).getStartingDestination();
+        doReturn(finalPlaceDTO).when(request).getFinalDestination();
+        doReturn(false).when(orderDestinationService).areDestinationsValid(anyList());
+        // then
+        assertThrows(BadRequestException.class, () -> orderService.createOrder(request));
+        verify(request, times(1)).getFinalDestination();
+        verify(request, times(1)).getStartingDestination();
+        verify(orderDestinationService, times(1)).areDestinationsValid(anyList());
+        verify(userClient, never()).getUserById(anyLong());
+        verify(orderDestinationService, never()).getOrderDestinationWithValidation(any());
+        verify(orderMapper, never()).mapOrderRequestToEntity(any());
+        verify(trackNumberGenerator, never()).generateTrackNumber(any(), any());
+        verify(orderRepository, never()).save(any());
+        verify(orderMapper, never()).mapOrderEntityToResponse(any());
+    }
+}
