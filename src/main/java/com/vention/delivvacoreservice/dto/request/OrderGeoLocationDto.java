d566ff90e4a35e4620e881015c75fc3319e93c8f
DeLivva <151529065+DeLivva@users.noreply.github.com>
2023-12-15 15:38:48 +0500

    added ci cd (#7)
    Co-authored-by: shakhzod9575 <shahzodelturayevepam@gmail.com>

--- src/main/java/com/vention/delivvacoreservice/dto/request/OrderGeoLocationDto.java
+++ src/main/java/com/vention/delivvacoreservice/dto/request/OrderGeoLocationDto.java
@@ -1,11 +1,17 @@
 package com.vention.delivvacoreservice.dto.request;
 
 import com.vention.delivvacoreservice.dto.GeolocationDTO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderGeoLocationDto extends GeolocationDTO {
     private String trackNumber;
 }
