c61e98261820eaaebb7298ce55abd6d0523ba534
Abbos Akramov <87889743+Abbos121@users.noreply.github.com>
2023-12-20 19:09:53 +0500

    Feature/ji 56 orders status for disputes (#21)
    * security config updated

--- src/main/java/com/vention/delivvacoreservice/config/SecurityConfig.java
+++ src/main/java/com/vention/delivvacoreservice/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.vention.delivvacoreservice.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,14 +23,16 @@ public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtFilter;
 
+    private static final List<String> AUTHORIZED_DOMAINS = List.of(
+            "delivva-dispute-env.eba-chhhwrqq.eu-north-1.elasticbeanstalk.com"
+    );
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(HttpMethod.GET, "/api/v1/orders/**").permitAll()
-                        .requestMatchers(HttpMethod.PUT, "/api/v1/orders/**").permitAll()
+                        .requestMatchers(new IpRequestMatcher()).permitAll()
                         .anyRequest().hasAnyRole("ADMIN", "USER")
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
@@ -46,5 +48,20 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    static class IpRequestMatcher implements RequestMatcher {
+        private final List<String> authorizedDomains;
+
+        IpRequestMatcher() {
+            this.authorizedDomains = AUTHORIZED_DOMAINS;
+        }
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            String clientDomain = request.getServerName();
+            return authorizedDomains.stream()
+                    .anyMatch(clientDomain::contains);
+        }
     }
 }
