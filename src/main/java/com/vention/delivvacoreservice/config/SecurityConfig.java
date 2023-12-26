9210d8c36347e88c6abf59079cdd0a364c4820ec
Abbos Akramov <abbosakramov121@gmail.com>
2023-12-26 16:28:27 +0500

    security config updated

--- src/main/java/com/vention/delivvacoreservice/config/SecurityConfig.java
+++ src/main/java/com/vention/delivvacoreservice/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
