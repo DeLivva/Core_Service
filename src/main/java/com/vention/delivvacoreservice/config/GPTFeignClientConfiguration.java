985ccaf27c8bea3512e51cd40f8c013b1e240a24
Abbos Akramov <abbosakramov121@gmail.com>
2024-01-07 21:57:42 +0500

    chatgpt was integrated to populate the database and some order invitations implemented

--- src/main/java/com/vention/delivvacoreservice/config/GPTFeignClientConfiguration.java
+++ src/main/java/com/vention/delivvacoreservice/config/GPTFeignClientConfiguration.java
@@ -2,20 +2,24 @@ package com.vention.delivvacoreservice.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 
 // If @Configuration annotations is written here, it will apply for all fiegn clients.
+@RequiredArgsConstructor
 public class GPTFeignClientConfiguration {
+
+    private final OpenApiConfig openApiConfig;
 
     @Bean
     public RequestInterceptor requestInterceptor() {
         return new CustomRequestInterceptor();
     }
 
-    private static class CustomRequestInterceptor implements RequestInterceptor {
+    private class CustomRequestInterceptor implements RequestInterceptor {
         @Override
         public void apply(RequestTemplate template) {
-            template.header("Authorization", "Bearer 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");
+            template.header("Authorization", "Bearer " + openApiConfig.getOpenApiSecretKey());
         }
     }
 }
