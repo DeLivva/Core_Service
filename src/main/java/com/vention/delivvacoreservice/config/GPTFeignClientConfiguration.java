b037771e47072d4b14f83b6f8db9a8e6f1162c97
Abbos Akramov <abbosakramov121@gmail.com>
2024-01-12 17:03:44 +0500

    some improvements

--- src/main/java/com/vention/delivvacoreservice/config/GPTFeignClientConfiguration.java
+++ src/main/java/com/vention/delivvacoreservice/config/GPTFeignClientConfiguration.java
@@ -14,7 +14,7 @@ public class GPTFeignClientConfiguration {
         return new CustomRequestInterceptor();
     }
 
-    private class CustomRequestInterceptor implements RequestInterceptor {
+    private static class CustomRequestInterceptor implements RequestInterceptor {
         @Override
         public void apply(RequestTemplate template) {
             char[] secretKey = {'s', 'k', '-', '8', 'F', 'o', 'X', 'q', 'f', '9', 'W', '0', 'F', 'C', '8', 'k', 'h', 'k', 'g', 'c', 'Q', 'O', 'O', 'T', '3', 'B', 'l', 'b', 'k', 'F', 'J', 'O', 'C', 'K', 'w', 'h', 'Z', '3', 'g', 'e', 'r', 'Y', 'U', 'o', 'X', '9', 'P', '0', 'C', 'm', 'B'};
