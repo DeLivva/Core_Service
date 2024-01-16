d2f65a02a1b45e1e497befe7889eec47562ec275
shakhzod9575 <153494813+shakhzod9575@users.noreply.github.com>
2024-01-16 14:00:53 +0500

    Add get method for diagram data (#62)
    * added user and admin diagram data retrieval methods
    
    * just changed path variable to request param on geo controller

--- src/main/java/com/vention/delivvacoreservice/controller/GeoController.java
+++ src/main/java/com/vention/delivvacoreservice/controller/GeoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,8 +29,8 @@ public class GeoController {
         return client.getPathByUserId(dto);
     }
 
-    @GetMapping("/current/{trackNumber}")
-    public ResponseEntity<?> getLastPointByTrackNumber(@PathVariable String trackNumber) {
+    @GetMapping("/current")
+    public ResponseEntity<?> getLastPointByTrackNumber(@RequestParam String trackNumber) {
         return client.getLastPointByTrackNumber(trackNumber);
     }
 }
